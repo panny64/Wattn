b7cbc85f661c3efee67f910d725c3554cca0963b
Johannes Pannermayr <johannes.pannermayr@web.d>
2018-06-16 17:17:33 +0200

    SchlagAnsage Bot

--- src/Players/Bot.java
+++ src/Players/Bot.java
@@ -5,9 +5,7 @@ import Cards.Card;
 import Cards.Farbe;
 import Cards.Schlag;
 
-
 import java.util.ArrayList;
-import java.util.Map;
 
 public class Bot extends Players {
 
@@ -18,7 +16,28 @@ public class Bot extends Players {
     public Schlag SchlagAnsagen() {
 
         int[] counters = {0, 0, 0, 0, 0, 0, 0, 0};  //7,8,9,10,U,O,K,S
-        return Schlag.NEUN;
+        Schlag[] schlag = {Schlag.SIEBEN, Schlag.ACHT, Schlag.NEUN, Schlag.ZEHN, Schlag.UNTER, Schlag.OBER, Schlag.KÖNIG, Schlag.SAU};
+
+        for(int i = 0; i < hand.size(); i++){
+            for (int j = 0; j < schlag.length; j++){
+                if (schlag[j] == hand.get(i).getSchlag()){
+                    counters[j]++;
+                }
+            }
+        }
+
+        int index = 0;
+        int maxvalue = 0;
+
+        for(int i = 0; i < schlag.length; i++){
+            if(counters[i] > maxvalue){
+                index = i;
+                maxvalue = counters[i];
+            }
+        }
+
+
+        return schlag[index];
     }
 
     public Farbe FarbeAnsagen() {
