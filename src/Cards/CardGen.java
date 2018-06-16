5382564df864e3ad30b85f46dfa1c4d97605269e
Johannes Pannermayr <johannes.pannermayr@web.d>
2018-06-16 18:53:15 +0200

    CardGen mischen implementiert

--- src/Cards/CardGen.java
+++ src/Cards/CardGen.java
@@ -1,6 +1,9 @@
 package Cards;
 
+import java.util.ArrayList;
 import java.util.Stack;
+import java.util.Random;
+
 
 public final class CardGen {
 
@@ -10,17 +13,54 @@ public final class CardGen {
     }
 
     public static Stack<Card> giveCards(){
-        Stack<Card> cards = new Stack<Card>();
+
+        ArrayList<Card> mix = new ArrayList<>();
 
         Schlag[] schlag = {Schlag.SIEBEN,Schlag.ACHT,Schlag.NEUN,Schlag.ZEHN,Schlag.UNTER,Schlag.OBER,Schlag.KÖNIG,Schlag.SAU};
         Farbe[] farbe = {Farbe.EICHEL,Farbe.GRAS,Farbe.HERZ,Farbe.SCHELLEN};
 
         for(int i = 0; i<schlag.length;i++){
             for(int j = 0;j<farbe.length;j++){
-                cards.push(new Card(farbe[j],schlag[i]));
+                mix.add(new Card(farbe[j],schlag[i]));
             }
         }
 
-        return cards;
+        Stack<Card> stack = shuffle(mix);
+
+
+
+        return stack;
+
     }
+
+    public static Stack<Card> shuffle(ArrayList<Card> sorted){
+
+        Random random = new Random();
+
+
+        for(int i = 0; i < 1000; i++){
+
+            int x = random.nextInt(31) + 1;         //31 maximum und 1 Minimum
+            int y = random.nextInt(31) + 1;         //same
+
+           Card first =  sorted.get(x);
+           Card second = sorted.get(y);
+           Card swap = second;
+           sorted.set(y,first);
+           sorted.set(x,swap);
+
+        }
+        Stack<Card> shuffled = new Stack<>();
+
+        while(!sorted.isEmpty()){
+            shuffled.add(sorted.remove(0));
+        }
+
+        return shuffled;
+
+    }
+
+
+
+
 }
