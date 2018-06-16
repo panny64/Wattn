9997677ac15491e3bcab4c8e2c55ad1b2ca02b8b
Andreas Strobl <strobland@r2d2.fim.uni-passau.de>
2018-06-16 17:15:35 +0200

    player can now choose which card he plays via console

--- src/Players/Human.java
+++ src/Players/Human.java
@@ -5,6 +5,7 @@ import Cards.Farbe;
 import Cards.Schlag;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Human extends Players {
 
@@ -20,5 +21,16 @@ public class Human extends Players {
         return Farbe.EICHEL;
     }
 
+    @Override
+    public Card playCard() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Numbers for 0 to " + (hand.size()-1));
 
+        for(int i = 0;i<hand.size();i++){
+            System.out.println(i + ": " + hand.get(i).getSchlag() + " " + hand.get(i).getFarbe());
+        }
+
+        int cardIndex = Integer.parseInt(scanner.next());
+        return hand.remove(cardIndex);
+    }
 }
