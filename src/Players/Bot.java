81806e496e2802fdb25942dfaa8a635556b61dff
Johannes Pannermayr <johannes.pannermayr@web.d>
2018-06-16 17:11:54 +0200

    Farbe Ansagen für Bot

--- src/Players/Bot.java
+++ src/Players/Bot.java
@@ -1,46 +1,51 @@
 package Players;
 
+
 import Cards.Card;
 import Cards.Farbe;
 import Cards.Schlag;
 
+
 import java.util.ArrayList;
 import java.util.Map;
 
-public class Bot extends Players{
+public class Bot extends Players {
 
-    public Bot(){
+    public Bot() {
         ArrayList<Card> hand = new ArrayList<>();
     }
 
-    public Schlag SchlagAnsagen(){
+    public Schlag SchlagAnsagen() {
 
-        int[] counters = {0,0,0,0,0,0,0,0};  //7,8,9,10,U,O,K,S
+        int[] counters = {0, 0, 0, 0, 0, 0, 0, 0};  //7,8,9,10,U,O,K,S
         return Schlag.NEUN;
     }
 
-    public Farbe FarbeAnsagen(){
+    public Farbe FarbeAnsagen() {
 
-        int[] counters = {0,0,0,0};     //Eichel,Gras,Herz,Schellen
+        int[] counters = {0, 0, 0, 0};     //Eichel,Gras,Herz,Schellen
+        Farbe[] farben = {Farbe.EICHEL, Farbe.GRAS, Farbe.HERZ, Farbe.SCHELLEN};
 
-        for(int i = 0; i<4;i++){
-            if(hand.get(i).getFarbe().equals(Farbe.EICHEL)){
-                counters[0]++;
+
+        for (int i = 0; i < hand.size(); i++) {
+            for (int j = 0; j < farben.length; j++) {
+                if (farben[j] == hand.get(i).getFarbe()) {
+                    counters[j]++;
+                }
             }
-            else if(hand.get(i).getFarbe().equals(Farbe.GRAS)){
-                counters[1]++;
-            }
-            else if(hand.get(i).getFarbe().equals(Farbe.HERZ)){
-                counters[2]++;
-            }
-            else counters[3]++;
         }
 
+        int index = 0;
+        int maxvalue= 0;
 
+        for (int i = 0; i < counters.length; i++) {
+            if(counters[i] > maxvalue){
+                index = i;
+                maxvalue = counters[i];
+            }
+        }
 
-
-
-        return Farbe.EICHEL;
+        return farben[index];
     }
 
 }
