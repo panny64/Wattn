dc4a3b7b6de7ba3f58647d61c9e3f985b06525a0
Johannes Pannermayr <johannes.pannermayr@web.d>
2018-06-16 16:36:24 +0200

    Bot Farbeansagen dummy

--- src/Players/Bot.java
+++ src/Players/Bot.java
@@ -5,6 +5,7 @@ import Cards.Farbe;
 import Cards.Schlag;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Bot extends Players{
 
@@ -13,10 +14,33 @@ public class Bot extends Players{
     }
 
     public Schlag SchlagAnsagen(){
+
+        int[] counters = {0,0,0,0,0,0,0,0};  //7,8,9,10,U,O,K,S
         return Schlag.NEUN;
     }
 
     public Farbe FarbeAnsagen(){
+
+        int[] counters = new {0,0,0,0};     //Eichel,Gras,Herz,Schellen
+
+        for(int i = 0; i<4;i++){
+            if(hand.get(i).getFarbe.equals(Farbe.EICHEL)){
+                counters[0]++;
+            }
+            else if(hand.get(i).getFarbe.equals(Farbe.GRAS)){
+                counters[1]++;
+            }
+            else if(hand.get(i).getFarbe.equals(Farbe.HERZ)){
+                counters[2]++;
+            }
+            else counters[3]++;
+        }
+
+        if()
+
+
+
         return Farbe.EICHEL;
     }
+
 }
