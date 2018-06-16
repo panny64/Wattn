326dd3b2e0148ef9bd26e13788d03ff75c4975a4
Andreas Strobl <strobland@r2d2.fim.uni-passau.de>
2018-06-16 16:02:37 +0200

    started game flow

--- src/BoardWattn.java
+++ src/BoardWattn.java
@@ -1,10 +1,14 @@
 import Cards.Card;
 import Cards.CardGen;
 import Players.*;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
 
 public class BoardWattn {
+
     Stack<Card> allCards;
     Players [] players;
     int [] scores;
@@ -14,25 +18,20 @@ public class BoardWattn {
 
     public BoardWattn(){
         players = new Players[4];
-        scores = new int[4];
         cardsPlayed = new ArrayList<Card>();
         ausspielerIndex = 0;
+        aktuellerSpieler = 0;
 
         players[0] = new Human();
         players[1] = new Bot();
         players[2] = new Bot();
         players[3] = new Bot();
 
-        allCards = CardGen.giveCards();
-
-        handOutCards();
-
-        for(int i = 0;i<players.length;i++){
-            players[i].printHand();
-        }
+        resetRound();
     }
 
-    public void tick(){/*
+    public void tick(){
+
         int playerWhoWon = -1;
 
         if(cardsPlayed.size() == 4){
@@ -43,12 +42,33 @@ public class BoardWattn {
             while(!cardsPlayed.isEmpty()){
                 cardsPlayed.remove(0);
             }
-
             aktuellerSpieler = ausspielerIndex;
         }else{
             playCard(aktuellerSpieler);
             aktuellerSpieler = (aktuellerSpieler+1)%4;
-        }*/
+        }
+
+        if(scores[0]+scores[2] >= 3){
+            System.out.println("Team 1 won.");
+            resetRound();
+        }else if(scores[1]+scores[3] >= 3){
+            System.out.println("Team 2 won.");
+            resetRound();
+        }
+
+        System.out.println(Arrays.toString(scores));
+    }
+    public void resetRound(){
+        for(int i = 0;i<players.length;i++){
+            players[i].emptyHands();
+        }
+        allCards = CardGen.giveCards();
+        handOutCards();
+
+        for(int i = 0;i<players.length;i++){
+            players[i].printHand();
+        }
+        scores = new int[4];
     }
 
     public void handOutCards(){
